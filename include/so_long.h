8f632a1e4176aa19eed8d56217e788d4a6b7cf3b
fenix144 <yan_1731@hotmail.com>
2022-04-29 13:02:41 -0400

    let's go

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,8 @@
 # include <stdio.h>
 # include <fcntl.h>
 # include <mlx.h>
-# include "../libft/libft.h"
+# include <math.h>
+# include "../src/libft/libft.h"
 
 enum {
 	ON_KEYDOWN = 2,
@@ -25,17 +26,69 @@ typedef struct s_criteria
 	int	E;
 }	t_criteria;
 
-char	**convertmaptostring(char *mapfile);
-int		countlines(char *mapfile);
-int		checkmap(char **map);
-int		validchar(char c);
-int		checkboundaries(char **map, int lines);
-int		checkbody(char **map, int lines);
-int		checkcriteria(char **map, int lines, t_criteria info);
-int		parsecriteria(t_criteria info);
-int		checkshape(char **map, int lines);
-void	checkarg(int argc);
-void	initinfo(t_criteria *info);
-void	errorhandling(int status);
+typedef struct	s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+	int		width;
+	int		height;
+}	t_data;
+
+typedef struct s_player
+{
+	t_data	img1;
+	t_data	img2;
+	t_data	img3;
+	t_data	img4;
+	t_data	img5;
+	t_data	img6;
+	int		x_pos;
+	int		y_pos;
+	int		steps;
+}	t_player;
+
+typedef struct s_env
+{
+	t_data	floor1;
+	t_data	floor2;
+	t_data	floor3;
+	t_data	floor4;
+	t_data	wall1;
+	t_data	wall2;
+	t_data	wall3;
+	t_data	wall4;
+	t_data	exit;
+	int		x_map;
+	int		y_map;
+}	t_env;
+
+typedef struct s_vars
+{
+	void	*mlx;
+	void	*win;
+}	t_vars;
+
+char			**convertmaptostring(char *mapfile);
+int				countlines(char *mapfile);
+int				checkmap(char **map);
+int				validchar(char c);
+int				checkboundaries(char **map, int lines);
+int				checkbody(char **map, int lines);
+int				checkcriteria(char **map, int lines, t_criteria info);
+int				parsecriteria(t_criteria info);
+int				checkshape(char **map, int lines);
+void			checkarg(int argc);
+void			initinfo(t_criteria *info);
+void			errorhandling(int status);
+unsigned int    get_colors(t_data *data, int x, int y);
+t_data    		image_scale_init(t_data *image, float scale, void *mlx);
+void			my_mlx_pixel_put(t_data *data, int x, int y, int color);
+void			get_player_pos(char **map, int *x_pos, int *y_pos);
+void			player_init(t_player *player, t_vars vars, char **map);
+void			my_xpm_file_to_image(char	*pathtoimg, t_vars vars, t_data *img);
+void			my_get_data_addr(t_data *img);
 
 #endif
